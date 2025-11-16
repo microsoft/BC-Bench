1fdb99539234243fda1a2182bb9b8e519b186d26
Sun Haoran <haoransun@microsoft.com>
2025-11-16 18:03:46 +0000

    add build rate to Braintrust

--- evaluator/scores.py
+++ evaluator/scores.py
@@ -4,3 +4,8 @@ from __future__ import annotations
 class ResolutionRate:
     def __call__(self, *, metadata: dict, **kwargs):
         return metadata.get("resolved", False)
+
+
+class BuildRate:
+    def __call__(self, *, metadata: dict, **kwargs):
+        return metadata.get("build", False)
