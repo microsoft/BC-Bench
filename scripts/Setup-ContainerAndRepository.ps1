48785c1f0ceb1015e36d16aadd2e31edd749d969
Jiawen Sun <t-jiawensun@microsoft.com>
2026-04-16 15:28:54 +0200

    Auto-detect counterfactual dataset path from InstanceId

--- scripts/Setup-ContainerAndRepository.ps1
+++ scripts/Setup-ContainerAndRepository.ps1
@@ -10,7 +10,7 @@ param(
     [string]$InstanceId,
 
     [Parameter(Mandatory = $false)]
-    [string]$DatasetPath = (Get-BCBenchDatasetPath),
+    [string]$DatasetPath,
 
     [Parameter(Mandatory = $false)]
     [string]$Country = "w1",
@@ -30,6 +30,11 @@ param(
     [Parameter(Mandatory = $false)]
     [switch]$SkipContainer
 )
+
+if (-not $DatasetPath) {
+    $datasetName = if ($InstanceId -match '__cf-\d+$') { "counterfactual.jsonl" } else { "bcbench.jsonl" }
+    $DatasetPath = Get-BCBenchDatasetPath -DatasetName $datasetName
+}
 
 [DatasetEntry[]] $entries = Get-DatasetEntries -DatasetPath $DatasetPath -Version $Version -InstanceId $InstanceId
 if ($InstanceId) {
