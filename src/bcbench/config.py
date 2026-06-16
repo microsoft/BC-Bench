2dad93f89e136dc394818e83f1c35980e1e41294
Haoran Sun (Business Central) <haoransun@microsoft.com>
2026-06-16 17:05:00 +0200

    OS agnostic hook

--- src/bcbench/config.py
+++ src/bcbench/config.py
@@ -41,7 +41,6 @@ class PathConfig:
     leaderboard_dir: Path
     agent_share_dir: Path
     hook_script_path: Path
-    hook_script_path_py: Path
 
     @classmethod
     def from_root(cls, root: Path) -> PathConfig:
@@ -57,7 +56,6 @@ class PathConfig:
             leaderboard_dir=root / "docs" / "_data",
             agent_share_dir=agent_share_dir,
             hook_script_path=agent_share_dir / "hooks" / "log-tool-usage.ps1",
-            hook_script_path_py=agent_share_dir / "hooks" / "log_tool_usage.py",
         )
 
 
